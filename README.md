54c020b843cd52f8b8c3981b9d4211a7fe4061dd
Steven Zhang <steven@pursuit.org>
2023-12-06 15:31:23 -0500

    Initial commit with README and .gitignore

---
+++ README.md
@@ -0,0 +1,17 @@
+# Variable Manipulation
+
+## Description
+Learn to manipulate and update variables.
+
+## Objective
+- Initialize and update variables to store different data types.
+- Console log the updated values.
+
+## Setup
+1. Fork and clone the repository.
+2. Navigate to the `01-08-variable-manipulation` directory.
+3. Fill in the `index.js` according to the instructions.
+
+## Test Cases
+- Variables should be correctly updated with new values.
+- Console log should display the updated variable values.
---
+++ index.js
@@ -0,0 +1,18 @@
+// Initialize variables
+let age = 25;
+let greeting = "Hello, ";
+let flag = false;
+
+// TODO: Update the value of 'age' by adding 5 to it.
+// Replace this line with your code.
+
+// TODO: Concatenate the string "World!" to 'greeting'.
+// Replace this line with your code.
+
+// TODO: Change the value of 'flag' to true.
+// Replace this line with your code.
+
+// Log Updated Values
+console.log(`Updated age: ${age}`);
+console.log(`Updated greeting: ${greeting}`);
+console.log(`Updated flag: ${flag}`);
